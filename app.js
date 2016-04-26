c611f3d4c876b94ca543c6e9e1a7ab14fd50e84f
Heley Chen <zccz14@outlook.com>
2016-04-27 02:11:33 +0800

    连接数据库

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+// Todo: Move Arguments to Config.js
+require('mongoose').connect('mongodb://localhost/unijudge'); 
 
 var app = express();
 

47a6661314df60cb0b05c9d434f572d0766a20f0
Heley Chen <zccz14@outlook.com>
2016-04-27 02:06:13 +0800

    初始化环境

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(require('express-session')({
+    secret: 'UNIJUDGE_SECRET_KEY',
+    key: 'UniJudgeId',
+    resave: false,
+    saveUninitialized: true,
+}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+module.exports = app;
